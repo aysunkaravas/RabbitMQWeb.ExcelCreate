269d150bf268dcc4eb0c6881e8f26de1c26a62fd
aysunkaravas <karavasaysun@gmail.com>
2024-03-15 12:59:58 +0300

    Add project files.

---
+++ RabbitMQWeb.ExcelCreate/Models/UserFile.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RabbitMQWeb.ExcelCreate.Models
+{
+    public enum FileStatus
+    {
+        Creating,
+        Complete
+    }
+
+
+
+    public class UserFile
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public FileStatus FileStatus { get; set; }
+        [NotMapped]
+        public string GetCreateDate => CreateDate.HasValue ? CreateDate.Value.ToShortDateString() : "-";   //null olup olmadığı kontrol edildi.
+
+       
+    }
+}
